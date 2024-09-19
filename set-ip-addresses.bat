c62e6627dda9481bfcff5ba979d7e365169566a4
Kerbalnut <Kerbalnut@users.noreply.github.com>
2024-09-19 14:31:20 -0700

    Added notes and tags throughout script
    To make it easier for others to identify what it does, the different sections, and how to use and update the script.

--- set-ip-addresses.bat
+++ set-ip-addresses.bat
@@ -1,5 +1,17 @@
 @ECHO OFF
 
+:: SUMMARY:
+:: Set multiple static IP addresses on an interface, or change it to DHCP.
+:: DESCRIPTION:
+:: Useful for programming multiple different IP camera brands & models, which can use many different default IP address ranges.
+:: Also useful in general for any kind of physical network hopping with a laptop.
+:: NOTES:
+:: Change the "_NET_INTERFACE_NAME" variable (below near the "MainVars" tag) to match the interface you want to change. Find out interface names by using the "ipconfig" command.
+:: Adjust the static IPs and subnet masks under the "SetStaticIPs" tag.
+:: EXAMPLE:
+:: Doulbe-click on this script to launch it. It will offer you a choice of setting the static IPs (below) or setting the interface to use DHCP.
+
+
 :RunAsAdministrator
 :: BatchGotAdmin International-Fix Code
 :: https://sites.google.com/site/eneerge/home/BatchGotAdmin
@@ -29,35 +41,41 @@ IF '%ERRORLEVEL%' NEQ '0' (
 :-------------------------------------------------------------------------------
 :: End Run-As-Administrator function
 
+:MainVars
 SET "_NET_INTERFACE_NAME=Ethernet"
 
-CHOICE /C SD /M "Set 'Ethernet' to [S]tatic IPs, or set as [D]HCP?"
+:StartScript
+CHOICE /C SD /M "Set '%_NET_INTERFACE_NAME%' to [S]tatic IPs, or set as [D]HCP?"
 IF ERRORLEVEL 2 GOTO SetDHCP & REM No./DHCP
 IF ERRORLEVEL 1 GOTO SetStaticIPs & REM Yes./Static
 :NOCHOICE
 
 :SetDHCP
 ECHO Setting DHCP:
-netsh interface ipv4 set address name="Ethernet" source=dhcp
+netsh interface ipv4 set address name="%_NET_INTERFACE_NAME%" source=dhcp
+::netsh interface ipv4 set address "%_NET_INTERFACE_NAME%" dhcp
+::netsh interface ip show config
 GOTO End
 
 :SetStaticIPs
 ECHO Setting Static IPs:
 REM netsh interface ipv4 set address name="Ethernet" static 192.168.1.25 255.255.255.0 192.168.1.1
-:: US Bank:
-netsh interface ipv4 add address "Ethernet" 192.168.1.25 255.255.255.0
-netsh interface ipv4 add address "Ethernet" 192.168.120.40 255.255.255.0
-:: Mayo:
-netsh interface ipv4 add address "Ethernet" 10.232.0.1 255.255.0.0
-netsh interface ipv4 add address "Ethernet" 10.233.52.1 255.255.0.0
-netsh interface ipv4 add address "Ethernet" 10.233.56.1 255.255.0.0
-netsh interface ipv4 add address "Ethernet" 10.233.60.1 255.255.0.0
-netsh interface ipv4 add address "Ethernet" 10.233.64.1 255.255.0.0
-netsh interface ipv4 add address "Ethernet" 10.233.68.1 255.255.0.0
-netsh interface ipv4 add address "Ethernet" 10.233.72.1 255.255.0.0
-netsh interface ipv4 add address "Ethernet" 10.233.76.1 255.255.0.0
 
+:: Network 1:
+netsh interface ipv4 add address "%_NET_INTERFACE_NAME%" 192.168.0.25 255.255.255.0
+netsh interface ipv4 add address "%_NET_INTERFACE_NAME%" 192.168.1.25 255.255.255.0
+netsh interface ipv4 add address "%_NET_INTERFACE_NAME%" 192.168.120.40 255.255.255.0
+:: Network 2:
+netsh interface ipv4 add address "%_NET_INTERFACE_NAME%" 10.232.0.1 255.255.0.0
+netsh interface ipv4 add address "%_NET_INTERFACE_NAME%" 10.233.52.1 255.255.0.0
+netsh interface ipv4 add address "%_NET_INTERFACE_NAME%" 10.233.56.1 255.255.0.0
+netsh interface ipv4 add address "%_NET_INTERFACE_NAME%" 10.233.60.1 255.255.0.0
+netsh interface ipv4 add address "%_NET_INTERFACE_NAME%" 10.233.64.1 255.255.0.0
+netsh interface ipv4 add address "%_NET_INTERFACE_NAME%" 10.233.68.1 255.255.0.0
+netsh interface ipv4 add address "%_NET_INTERFACE_NAME%" 10.233.72.1 255.255.0.0
+netsh interface ipv4 add address "%_NET_INTERFACE_NAME%" 10.233.76.1 255.255.0.0
 
+GOTO End
 
 :End
 ECHO End of script.
